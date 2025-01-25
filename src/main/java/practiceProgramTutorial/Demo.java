c42b7334dc6683b69baf4fde6626f43eb9128334
Swapnil Valvi <swapnilvalvi17@gmail.com>
2025-01-25 18:03:02 +0530

    Swapnil has recieved commands

--- src/main/java/practiceProgramTutorial/Demo.java
+++ src/main/java/practiceProgramTutorial/Demo.java
@@ -3,6 +3,8 @@ package practiceProgramTutorial;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Frame work created");
+		
+		System.out.println("Code recieved by swapnil");
 	}
 
 }
