bdbdba7eaf217772c46941c6d0e8a838ce64e5ee
Swapnil Valvi <swapnilvalvi17@gmail.com>
2025-01-25 20:29:20 +0530

    swapnil commits

--- src/main/java/practiceProgramTutorial/Demo.java
+++ src/main/java/practiceProgramTutorial/Demo.java
@@ -3,12 +3,14 @@ package practiceProgramTutorial;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Frame work created");
-<<<<<<< HEAD
+
 		
 		System.out.println("Code recieved by swapnil");
-=======
+
 		System.out.println("Frame work Created by rutuja side");
->>>>>>> 2f34682e7fba1aeb320ade936d0350caa6ab4fd8
+		
+		System.out.println("Code is committed again");
+
 	}
 
 }
